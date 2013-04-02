8d57269fe9d0ef7cdfd6b389e2c6c91e24ddbae8
Ation <gation@gmail.com>
2013-04-03 00:15:29 +0300

    Add node class file

---
+++ spell_checker/dictionary_tree_node.h
@@ -0,0 +1,18 @@
+#ifndef __DICTIONARY_TREE_NODE_H__
+#define __DICTIONARY_TREE_NODE_H__
+
+/*
+	file: 
+		dictionary_tree_node.h
+	Description:
+		Contain class that represent node in string tree;
+*/
+
+namespace spell_checker {
+
+class DictionaryTreeNode {
+};
+
+};
+
+#endif //__DICTIONARY_TREE_NODE_H__
