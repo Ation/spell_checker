90aec0dddef5dbf8281b1dfdf74de224f48c4ab4
Ation <gation@gmail.com>
2013-09-16 19:25:20 +0200

    simple UT for corrections

--- testing/find_word_testing.cpp
+++ testing/find_word_testing.cpp
@@ -9,9 +9,13 @@ using namespace spell_checker;
 using namespace std;
 
 static bool FindWord();
+static bool FindWordWhenCorrectionsArePossible();
+static bool FindAllCorrections();
 
 static TestCase	cases[] = {
-    {"Find word", FindWord}
+    {"Find word", FindWord},
+    {"Find word when corrections possible", FindWordWhenCorrectionsArePossible},
+    {"Find all corrections", FindAllCorrections}
 };
 
 void
@@ -56,4 +60,68 @@ bool FindWord()
     }
 
     return true;
+}
+
+bool FindWordWhenCorrectionsArePossible()
+{
+    const string target("worda");
+    const string opt1("wordb");
+    const string opt2("word");
+    const string opt3("wordab");
+
+    SpellingDictionary  dict;
+
+    dict.AddWord(target);
+    dict.AddWord(opt1);
+    dict.AddWord(opt2);
+    dict.AddWord(opt3);
+
+    vector<string>  searchResult = dict.GetWordOptions(target, 2);
+
+    if (searchResult.size() != 1) {
+        return false;
+    }
+
+    if (searchResult[0] != target) {
+        return false;
+    }
+
+    return true;
+}
+
+bool FindAllCorrections()
+{
+    const string target("worda");
+    const string opt1("wordb");
+    const string opt2("word");
+    const string opt3("wordab");
+
+
+    bool containOpt2 = false;
+    bool containOpt3 = false;
+
+    SpellingDictionary  dict;
+
+    dict.AddWord(opt1);
+    dict.AddWord(opt2);
+    dict.AddWord(opt3);
+
+    vector<string>  searchResult = dict.GetWordOptions(target, 2);
+
+    // replace symbol - two operations
+    if (searchResult.size() != 2) {
+        return false;
+    }
+
+    for (int i=0; i < 2; i++) {
+        if (searchResult[i] == opt2) {
+            containOpt2 = true;
+        }
+
+        if (searchResult[i] == opt3) {
+            containOpt3 = true;
+        }
+    }
+
+    return containOpt2 && containOpt3;
 }
