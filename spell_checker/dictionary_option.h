9827120a9ca4ce2b7f24c4690daafd37999509d6
Ation <gation@gmail.com>
2013-09-29 15:43:45 +0200

    Parametrize option

--- spell_checker/dictionary_option.h
+++ spell_checker/dictionary_option.h
@@ -1,7 +1,6 @@
 #ifndef __DICTIONARY_OPTION_H__
 #define __DICTIONARY_OPTION_H__
 
-#include <string>
 #include <memory>
 #include <list>
 
@@ -9,35 +8,101 @@
 
 namespace spell_checker {
 
+template<typename __symbol_traits>
 class DictionaryOption {
 public:
-    DictionaryOption(DictionaryTreeNode *root, int max_string_length);
+    typedef typename DictionaryTreeNode<__symbol_traits> node_type;
+    typedef typename DictionaryOption<__symbol_traits> my_type;
 
-	DictionaryOption(const DictionaryOption &src);
+    typedef typename __symbol_traits::string_type   string_type;
 
-	~DictionaryOption() {}
+    explicit DictionaryOption(node_type *root) : m_corrections(0), m_word_index(0),
+      m_node(root), m_last_operation(NotProceededOperation)
+    {}
 
-	bool		corrections_allowed(int	allowed_correction_count) const;
+    DictionaryOption(const my_type &src) :
+      m_corrections(src.m_corrections), m_word_index(src.m_word_index), m_node(src.m_node),
+      m_last_operation(src.m_last_operation)
+    {}
 
-    void		InsertCorrections(std::list<DictionaryOption> &c, const std::string &word) const;
+    ~DictionaryOption() {}
 
-    bool	ReachEnd(const std::string &word) const;
+    bool    corrections_allowed(int allowed_correction_count) const {
+        return m_corrections < allowed_correction_count;
+    }
 
-    bool	WordComplete() const;
+    void    InsertCorrections(std::list<my_type> &c, const string_type &word) const {
+        // add possible insertions
+        std::vector<node_type*> childs = node->getChilds();
+        for (std::vector<node_type*>::iterator i_node = childs.begin(); i_node != childs.end(); ++i_node) {
+            if (0 == (*i_node)) {
+                continue;
+            }
 
-    bool	MoveToNextSymbol(const std::string &word);
+            if ((*i_node)->get_node_symbol() == node->get_node_symbol()) {
+                continue;
+            }
 
-	std::string		GetString() const;
+            my_type op(*this);
+
+            op.insert_node(*i_node);
+
+            c.push_back(op);
+        }
+
+        // add removal if make sence
+        if (word_index < word.length()) {
+            my_type op(*this);
+
+            op.skip_symbol();
+
+            c.push_back(op);
+        }
+    }
+
+    bool    ReachEnd(const string_type &word) const {
+        return m_word_index == word.length();
+    }
+
+    bool    WordComplete() const {
+        return m_node->could_be_last();
+    }
+
+    bool	MoveToNextSymbol(const string_type &word) {
+        node_type next_symbol_node = m_node->get_child(word[m_word_index]);
+
+        if (NULL != next_symbol_node) {
+            m_node = next_symbol_node;
+            // next symbol exists
+            ++word_index;
+            m_last_operation = SymbolFindOperation;
+            return true;
+        }
+
+        return false;
+    }
+
+    string_type		GetString() const {
+        return m_node->get_node_string();
+    }
 
 private:
-    void		skip_symbol();
+    void    skip_symbol() {
+        m_last_operation = RemoveOperation;
+        ++m_corrections;
+        ++m_word_index;
+    }
 
-    void		insert_node(DictionaryTreeNode	*new_node);
+    void    insert_node(node_type	*new_node) {
+        m_last_operation = InsertOperation;
+        ++m_corrections;
+        m_node = new_node;
+    }
 
 	int								m_corrections;
-	size_t							word_index;
+    size_t							m_word_index;
 
-    DictionaryTreeNode	*node;
+    node_type	*m_node;
 
 	enum _last_operation {
 		InsertOperation,
